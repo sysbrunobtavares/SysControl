6beca2f0dfd0f4f12f6b1a02efbd14ed8b3b9679
Bruno Tavares <brunobtavares@outlook.com>
2021-08-06 11:30:57 -0300

    Init

---
+++ sysUpdate.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+:: Set UTF-8 enconding
+:: chcp 65001
+
+:: Mensagem
+ECHO +---------------------------------+
+ECHO ^|                                 ^|
+ECHO ^| Atualizando o sistema. Aguarde! ^|
+ECHO ^|                                 ^|
+ECHO +---------------------------------+
+
+:: Ping usado como sleep time por 5seg
+ping 127.0.0.1 -n 6 > nul
+
+:: Faz a requisição para atualizar
+git fetch
+git reset --hard
+git pull
+
+:: Inicia o systema após atualização
+start Agendasys.exe
+exit
